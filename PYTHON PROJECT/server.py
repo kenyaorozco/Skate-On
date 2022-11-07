f70a69107ce43bf399ad86cfcc6a08b8073f6527
Kenya Orozco <109471756+kenyaorozco@users.noreply.github.com>
2022-11-07 10:48:46 -0800

    Update server.py

--- PYTHON PROJECT/server.py
+++ PYTHON PROJECT/server.py
@@ -3,13 +3,8 @@ from flask_app import app
 from flask_app.controllers import display_controllers
 from flask_app.controllers import dashboard_controllers
 # EDIT CONTROLLERS
-# FLASK_APP_API_KEY=RANDOMAPIKEY
 
-# client ID
-# xDt4_emqahl9YBaRVGDkvw
 
-# API KEY
-# hQD93KZujlrqW8RqMZh70FJw99u_bO-K5YXJE0aQG_ob5YQcczn2J-Xvyh7hmzifQ8cXlkHz8aENf8O9SFkAaO21CJPlUV7_cW5wv38-mLF7d9IQAncylG8FbNsGY3Yx
 
 import requests
 import json
